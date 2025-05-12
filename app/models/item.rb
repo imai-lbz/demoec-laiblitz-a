dfa0448c7c639e0370c7ea7551f0bb359fe82fab
ryura-kinjo <kinjo.ryura@laiblitz.co.jp>
2025-05-12 18:22:56 +0900

    itemモデルにバリデーションを設定した。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  validates :name,         presence: true
+  validates :description,  presence: true
+  validates :category_id,  presence: true
+  validates :condition_id, presence: true
+  validates :price,        presence: true, format: { with: /\A[0-9]+\z/ }, numericality: {  only_integer: true,
+                                                                                            greater_than_or_equal_to: 300,
+                                                                                            less_than_or_equal_to: 9_999_999 }
 end
