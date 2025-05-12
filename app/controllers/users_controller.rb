97244892723f18fdf39a2a98e846551615e4c878
imai-lbz <imai.atsuhiro@laiblitz.co.jp>
2025-05-12 19:31:23 +0900

    新規登録機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,13 @@ class UsersController < ApplicationController
 
   def new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
 end
