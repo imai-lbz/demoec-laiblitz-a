123498e40bce835c6ebbf18b09ff94d0ea8b28fe
imai-lbz <imai.atsuhiro@laiblitz.co.jp>
2025-05-12 17:39:45 +0900

    itemテーブルを取り込むためのマイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_05_12_052201) do
+ActiveRecord::Schema[7.0].define(version: 2025_05_12_052251) do
   create_table "items", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.integer "price", null: false
@@ -19,8 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2025_05_12_052201) do
     t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
 
-ActiveRecord::Schema[7.0].define(version: 2025_05_12_052251) do
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -37,7 +37,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_05_12_052251) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
 
 end
