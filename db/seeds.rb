8fe14e4ec512b60935363c9dde352916ac345bfa
ryura-kinjo <kinjo.ryura@laiblitz.co.jp>
2025-05-13 12:09:51 +0900

    テスト用の管理者アカウントを作成できる設定をした

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# db/seeds.rb
+
+unless User.exists?(email: 'test@admin.com')
+  User.create!(
+    email: 'test@admin.com',
+    password: 'passw0rd',
+    password_confirmation: 'passw0rd',
+    nickname: 'Admin1',
+    kanji_family_name: '山田',
+    kanji_given_name: '太郎',
+    katakana_family_name: 'ヤマダ',
+    katakana_given_name: 'タロウ',
+    birthday: Date.new(2000, 1, 1)
+  )
+  puts 'テスト管理者アカウントを作成しました: test@admin.com'
+end
