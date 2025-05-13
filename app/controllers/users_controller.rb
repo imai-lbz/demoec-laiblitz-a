b0ea91ee0e99c4bc8ef7d5b0612b02b70d5ccc79
imai-lbz <imai.atsuhiro@laiblitz.co.jp>
2025-05-13 15:00:28 +0900

    ログアウトボタンの有効化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,18 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to users_path
+      redirect_to items_path
     else
       render :new
     end
+  end
+
+  def destroy
   end
 end
