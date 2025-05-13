d393b16bfa0642888c70f3ddbacbc67e8f234383
ryura-kinjo <kinjo.ryura@laiblitz.co.jp>
2025-05-13 10:50:51 +0900

    item.imageを必須に設定した

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :image
 
+  validates :image,        presence: true
   validates :name,         presence: true
   validates :description,  presence: true
   validates :category_id,  presence: true
