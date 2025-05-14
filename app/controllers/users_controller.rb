f5a75e32bce0117b856bafdb353f96b5a4dd0820
imai-lbz <imai.atsuhiro@laiblitz.co.jp>
2025-05-14 18:47:20 +0900

    登録ユーザーの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,22 @@
 class UsersController < ApplicationController
   def index
-    # @users = User.all
+    @users = User.all
+    render 'admin_users/index'
   end
 
   def new
     @user = User.new
   end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to items_path
-    else
-      render :new
-    end
-  end
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     redirect_to items_path
+  #   else
+  #     render :new
+  #   end
+  # end
 
-  def destroy
-  end
+  # def destroy
+  # end
 end
