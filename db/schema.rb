7c1f24720d523be9ef0688254c71693b38ebda13
ryura-kinjo <kinjo.ryura@laiblitz.co.jp>
2025-05-14 10:21:20 +0900

    admin_flagを追加したマイグレーションを戻した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_05_13_072431) do
+ActiveRecord::Schema[7.0].define(version: 2025_05_12_102604) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -63,8 +63,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_05_13_072431) do
     t.date "birthday", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin_flag"
-    t.index ["admin_flag"], name: "index_users_on_admin_flag"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
