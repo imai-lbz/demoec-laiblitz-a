d8930309817a7c8ff1f1e5d85c25effe3d543b8b
kinoshita-0429 <kinoshita.masaki@laiblitz.co.jp>
2025-05-15 16:45:14 +0900

    ログイン状態の一般ユーザーはトップページに遷移する、ログアウト状態のユーザーはログインページに遷移する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   # ログインしていない場合はログインページに強制遷移させる。
   # TODO: 管理者ユーザーで実際に挙動を確認する必要がある
-  before_action :authenticate_user!,       only: [:new]
-  before_action :authenticate_admin_user!, only: [:new]
+  before_action :authenticate_user!,       only: [:new, :edit]
+  before_action :authenticate_admin_user!, only: [:new, :edit]
 
   # トップページ
   def index
