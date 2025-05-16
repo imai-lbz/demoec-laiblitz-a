420ff59253621477805543a66b6e3552801acce0
ryura-kinjo <kinjo.ryura@laiblitz.co.jp>
2025-05-16 15:57:35 +0900

    createメソッドで購入ができるようにした

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,8 @@
 class OrdersController < ApplicationController
   def index
     gon.public_key = ENV['PAYJP_PUBLIC_KEY']
-    @item = Item.find_by(params[:item_id])
+    @item = Item.find(params[:item_id])
+    # binding.pry
     if @item.present?
       @order = Order.new(user: current_user, item: @item)
       @order.build_delivery_address(
@@ -18,35 +19,50 @@ class OrdersController < ApplicationController
   end
 
   def create
+    # TODO: chargeのエラーを@orderに追加して返すことでフロントでエラーを表示する
+
     @order = Order.new(order_params)
-    return unless @order.valid?
+
+    return render_index_with_item(alert: '入力内容に誤りがあります。') unless @order.valid?
 
     Payjp.api_key = ENV['PAYJP_SECRET_KEY'] # 自身のPAY.JPテスト秘密鍵を記述しましょう
-    test_charge = Payjp::Charge.create(
-      amount: @order.item.price, # 商品の値段
-      card: order_params[:token], # カードトークン
-      currency: 'jpy' # 通貨の種類（日本円）
-    )
+
+    begin
+      charge = Payjp::Charge.create(
+        amount: @order.item.price, # 商品の値段
+        card: order_params[:token], # カードトークン
+        currency: 'jpy' # 通貨の種類（日本円）
+      )
+    rescue Payjp::CardError => e
+      return render_index_with_item(alert: "カードエラー: #{e.message}")
+    rescue StandardError => e
+      return render_index_with_item(alert: "予期せぬエラーが発生しました: #{e.message}")
+    end
 
     # binding.pry
 
-    # if @order.save
-    #   redirect_to root_path, notice: '注文が完了しました。'
-    # else
-    #   flash.now[:alert] = '注文に失敗しました。入力内容をご確認ください。'
-    #   @item = Item.find_by(params[:item_id])
-    #   render :index, status: :unprocessable_entity # index ビューを再表示
-    # end
+    if charge&.paid
+      @order.save!
+      return redirect_to root_path, notice: '注文が完了しました。'
+    end
+
+    render_index_with_item(alert: '支払いに失敗しました。もう一度お試しください。')
   end
 
   private
 
+  def render_index_with_item(status: :unprocessable_entity, alert: nil)
+    @item = Item.find(params[:item_id])
+    flash.now[:alert] = alert if alert.present?
+    render :index, status: status
+  end
+
   def order_params
     params.require(:order).permit(
-      :user_id, :item_id,
+      :user_id,
       delivery_address_attributes: [
         :postal_code, :prefecture_id, :city, :address1, :building, :phone_number
       ]
-    ).merge(token: params[:token])
+    ).merge(token: params[:token], item_id: params[:item_id])
   end
 end
