cfa58e2ad9bef18fd6209d3bb3eddef3d7330c1d
ryura-kinjo <kinjo.ryura@laiblitz.co.jp>
2025-05-19 15:09:23 +0900

    購入されたアイテムを削除できるようにした

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
                                                                                             greater_than_or_equal_to: 300,
                                                                                             less_than_or_equal_to: 9_999_999 }
 
-  has_many :orders
+  has_many :orders, dependent: :destroy
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
