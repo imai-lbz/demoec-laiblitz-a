7473134efa6d7da522f469dba3d899a43699595a
ryura-kinjo <kinjo.ryura@laiblitz.co.jp>
2025-05-19 11:15:21 +0900

    destroyアクションを実装した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,14 @@ class UsersController < ApplicationController
     render 'admin_users/index'
   end
 
-  def destroy(user = User.find(params[:id]))
+  def destroy
+    user = User.find_by(id: params[:id])
+    if user
+      user.destroy
+      redirect_to users_path, notice: "#{user.nickname} を削除しました。"
+    else
+      redirect_to users_path, alert: '指定されたユーザーは存在しません。'
+    end
   end
 
   # 管理者を表すadmin_flagカラムはここで設定するべきではない
