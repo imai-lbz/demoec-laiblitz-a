d0a08c566616b9d24fe4a3ab68818fd906b53f05
ryura-kinjo <kinjo.ryura@laiblitz.co.jp>
2025-05-20 15:41:40 +0900

    ユーザーにポイントカラムを追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_15_055339) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_20_063701) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -86,6 +86,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_15_055339) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin_flag"
+    t.integer "point", default: 0, null: false
     t.index ["admin_flag"], name: "index_users_on_admin_flag"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
