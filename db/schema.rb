ff09d147292b4d2e30bde3e42f4ecb4d11a89667
ryura-kinjo <kinjo.ryura@laiblitz.co.jp>
2025-05-20 16:04:02 +0900

    orderにused_pointカラムを追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_20_063701) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_20_065810) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -67,6 +67,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_20_063701) do
     t.bigint "item_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "used_point", default: 0, null: false
     t.index ["item_id"], name: "index_orders_on_item_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
