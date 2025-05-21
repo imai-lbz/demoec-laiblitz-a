500fd0f8606e8cba89ad5e67cd965a024b2934ae
ryura-kinjo <kinjo.ryura@laiblitz.co.jp>
2025-05-21 10:09:51 +0900

    couponにバリデーションを設定した

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,2 +1,7 @@
 class Coupon < ApplicationRecord
+  validates :name, presence: true
+  validates :discount_rate,
+            presence: true,
+            numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 100 }
+  validates :expires_at, presence: true
 end
