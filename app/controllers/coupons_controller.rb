90fd81d94bbfa464d78a62550dc0f9055f6de07f
ryura-kinjo <kinjo.ryura@laiblitz.co.jp>
2025-05-21 10:51:04 +0900

    クーポン登録できるようにした

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -8,5 +8,17 @@ class CouponsController < ApplicationController
   end
 
   def create
+    @coupon = Coupon.new(coupon_params)
+    if @coupon.save
+      redirect_to coupons_path, notice: 'クーポンが登録されました'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def coupon_params
+    params.require(:coupon).permit(:name, :discount_rate, :expires_at)
   end
 end
