a5d97fb550202bb03a024a00b5e092c0af867003
ryura-kinjo <kinjo.ryura@laiblitz.co.jp>
2025-05-21 10:08:01 +0900

    couponモデルを作成した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_20_065810) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_21_010637) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_20_065810) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "coupons", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "discount_rate", null: false
+    t.datetime "expires_at", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "delivery_addresses", charset: "utf8mb3", force: :cascade do |t|
