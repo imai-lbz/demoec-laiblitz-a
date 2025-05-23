6d6fe5427d58e24f72c596feed468cf625797ffd
ryura-kinjo <kinjo.ryura@laiblitz.co.jp>
2025-05-23 15:36:23 +0900

    バナー表示機能を作成した

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,7 @@ class ItemsController < ApplicationController
   # トップページ
   def index
     @items = Item.order(created_at: :desc)
+    @promotions = Promotion.all.order(created_at: :desc)
   end
 
   # 商品管理・一覧ページ
