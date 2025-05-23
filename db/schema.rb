36bb074a7b923a56994fdad27287e95017ab0da3
ryura-kinjo <kinjo.ryura@laiblitz.co.jp>
2025-05-23 16:34:02 +0900

    noticeモデルを作成した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_23_052025) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_23_073306) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -83,6 +83,13 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_23_052025) do
     t.text "description", null: false
     t.integer "condition_id", null: false
     t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "notices", charset: "utf8mb3", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
