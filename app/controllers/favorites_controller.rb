e3fd981f8736777f76cc905e318c2cb767040575
imai-lbz <imai.atsuhiro@laiblitz.co.jp>
2025-05-26 19:20:25 +0900

    詳細画面のハート修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,36 +1,33 @@
-# class FavoritesController < ApplicationController
-#   def create
-#     item = Item.find(params[:item_id])
-#     favorite = current_user.favorites.new(item_id: item.id)
-#     favorite.save
-#     # リクエスト送信元のページ（商品詳細画面）に戻る
-#     redirect_to request.referer
-#   end
-
-#   def destroy
-#     item = Item.find(params[:item_id])
-#     favorite = current_user.favorites.find_by(item_id: item.id)
-#     favorite.destroy
-#     redirect_to request.referer
-#   end
-# end
-
 class FavoritesController < ApplicationController
   before_action :set_item
 
   def create
     current_user.favorites.create!(item: @item)
+
     respond_to do |format|
       format.html { redirect_to request.referer }
-      format.turbo_stream
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          "favorite_item_#{@item.id}",
+          partial: partial_for(request.referer),
+          locals: { item: @item }
+        )
+      end
     end
   end
 
   def destroy
     current_user.favorites.find_by(item: @item)&.destroy
+
     respond_to do |format|
       format.html { redirect_to request.referer }
-      format.turbo_stream
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          "favorite_item_#{@item.id}",
+          partial: partial_for(request.referer),
+          locals: { item: @item }
+        )
+      end
     end
   end
 
@@ -38,5 +35,16 @@ class FavoritesController < ApplicationController
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  # リファラー（戻り元URL）に応じて部分テンプレート名を返す
+  def partial_for(referer_url)
+    # 詳細画面のパスが含まれていれば _btn（詳細用）を、
+    # それ以外なら _btn_for_products（一覧用）を返す
+    if referer_url&.include?(item_path(@item))
+      'favorites/btn'
+    else
+      'favorites/btn_for_products'
+    end
   end
 end
